5e4511d1975f1b19a476a71966bc0b74f638e588
mBasak03 <mayukhbasak2003@gmail.com>
2024-12-03 23:33:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className=" flex items-between justify-between mx-[100px]">
+      <Button className=" bg-white border-2 hover:bg-gray-200">
+        <p className=" text-blue-500 ">
+
+        hdbbd
+        </p>
+        </Button>
+    </div>
+  )
+}
